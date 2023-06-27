fe1444033c48150cbcaa3fafc5668649a2fa9527
Onegodx <113604166+Onegodx@users.noreply.github.com>
2023-06-27 02:25:40 +0200

    Добавьте файлы проекта.

---
+++ WpfApp7/CodeFile1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            LinkedList<double> list = new LinkedList<double>();
+            list.AddLast(-2.5);
+            list.AddLast(-1.0);
+            list.AddLast(3.5);
+            list.AddLast(2.0);
+            list.AddLast(1.0);
+
+            
+            LinkedListNode<double> node = list.First;
+            while (node != null && node.Value <= 0)
+            {
+                node = node.Next;
+            }
+            if (node != null && node != list.First)
+            {
+                list.Remove(node.Previous);
+            }
+
+            
+            foreach (var item in list)
+            {
+                Console.Write("{0} ", item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace LinkedListWpfExample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            
+            LinkedList<double> list = new LinkedList<double>();
+            list.AddLast(-2.5);
+            list.AddLast(-1.0);
+            list.AddLast(3.5);
+            list.AddLast(2.0);
+            list.AddLast(1.0);
+
+            
+            LinkedListNode<double> node = list.First;
+            while (node != null && node.Value <= 0)
+            {
+                node = node.Next;
+            }
+            if (node != null && node != list.First)
+            {
+                list.Remove(node.Previous);
+            }
+
+            
+            lblResult.Content = string.Join(" ", list.Select(x => x.ToString()));
+        }
+    }
+}
